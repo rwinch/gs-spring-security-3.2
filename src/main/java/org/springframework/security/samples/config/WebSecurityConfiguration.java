51219cbbf76b3b9fd9cf842d4268142a2814213f
Rob Winch <rwinch@vmware.com>
2013-09-08 14:54:15 -0500

    Admin for H2

--- src/main/java/org/springframework/security/samples/config/WebSecurityConfiguration.java
+++ src/main/java/org/springframework/security/samples/config/WebSecurityConfiguration.java
@@ -15,6 +15,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 			.authorizeRequests()
+				.antMatchers("/h2/**").hasRole("ADMIN")
 				.antMatchers("/resources/**","/signup").permitAll()
 				.anyRequest().authenticated()
 				.and()
