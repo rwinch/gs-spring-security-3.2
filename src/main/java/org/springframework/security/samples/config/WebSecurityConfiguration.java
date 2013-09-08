738b94fceabaaa62c9cc7c51f31d6aed9b74461e
Rob Winch <rwinch@vmware.com>
2013-09-08 17:11:05 -0500

    Custom Authentication

--- src/main/java/org/springframework/security/samples/config/WebSecurityConfiguration.java
+++ src/main/java/org/springframework/security/samples/config/WebSecurityConfiguration.java
@@ -1,21 +1,23 @@
 package org.springframework.security.samples.config;
 
 
-import javax.sql.DataSource;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.samples.security.UserRepositoryUserDetailsService;
 
 @Configuration
 @EnableWebSecurity
+@ComponentScan(basePackageClasses=UserRepositoryUserDetailsService.class)
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Autowired
-	private DataSource dataSource;
+	private UserDetailsService userDetailsService;
 
 	@Order(1)
 	@Configuration
@@ -64,10 +66,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void registerAuthentication(AuthenticationManagerBuilder auth) throws Exception {
 		auth
-			.jdbcAuthentication()
-				.dataSource(dataSource)
-				.usersByUsernameQuery("select email, password, true from user where email = ?")
-				.authoritiesByUsernameQuery("select email, 'ROLE_USER' from user where email = ?");
+			.userDetailsService(userDetailsService);
 	}
 	// @formatter:on
 }
